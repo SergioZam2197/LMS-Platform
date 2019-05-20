11a2363fb73b4e94710c32c853830f65864ee29c
SergioZam2197 <50304441+SergioZam2197@users.noreply.github.com>
2019-05-20 17:23:07 -0400

    Update book.js

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -5,13 +5,7 @@ const bookController = require('../controllers/book');
 const isAuth = require('../middleware/is-auth');
 const router = express.Router();
 
-router.post('/students', studentController.postStudentInfo);
 
-router.post('/search', studentController.getStudentInfo);
-
-router.post('/newBook', studentController.postBookinfo);
-
-router.post('/books', bookController.getBookInfo);
 
 module.exports = router;
 
