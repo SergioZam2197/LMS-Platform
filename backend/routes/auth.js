ab926d4092553cf577b4727f42fda090e9360ac2
SergioZam2197 <50304441+SergioZam2197@users.noreply.github.com>
2019-05-20 17:22:46 -0400

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,7 +6,17 @@ const authController = require('../controllers/auth');
 
 const router = express.Router();
 
-
+router.post('/signup', [
+    body('email')
+        .isEmail()
+        .withMessage('please enter a valid email addres')
+        .normalizeEmail(),
+    body('password')
+        .trim()
+        .isLength({min: 5}),
+    body('name')
+        .trim()
+], authController.signup);
 
 router.post('/login', authController.login);
 
